cd43b55e69e8a390d9292c50724bfb5935cb0851
Johnnnvictor <victo_muh@hotmail.com>
2025-03-03 16:02:10 -0300

    Initial commit

---
+++ desafio-avanade-dio/src/main/java/me/dio/desafio_avanade_dio/DesafioAvanadeDioApplication.java
@@ -0,0 +1,13 @@
+package me.dio.desafio_avanade_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioAvanadeDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioAvanadeDioApplication.class, args);
+	}
+
+}
